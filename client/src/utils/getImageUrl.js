a1e0bd3f401376f6fc3a2561403e168c45789351
souravxyz <srivastavasourav68689@gmail.com>
2025-07-08 16:04:50 +0530

    image fix

--- client/src/utils/getImageUrl.js
+++ client/src/utils/getImageUrl.js
@@ -1,4 +1,4 @@
 export const getImageUrl = (fileName) => {
   if (!fileName) return "https://www.gravatar.com/avatar/default?s=200";
-  return `http://localhost:6900/uploads/profilePics/${fileName}`;
+  return `https://final-project-sourav-server.onrender.com/uploads/profilePics/${fileName}`;
 };
